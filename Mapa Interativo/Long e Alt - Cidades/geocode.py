1abf5d887680297b8b26a034f4e25c220789a867
Gustavo Gomes <gustavo.gomes012@gmail.com>
2020-09-01 23:39:49 -0300

    PROGRAMA FEITO - GEOCODE

---
+++ Mapa Interativo/Long e Alt - Cidades/geocode.py
@@ -0,0 +1,25 @@
+import geopandas as gpds
+import pandas as pds # Para carregar a planilha com os endereços
+
+ler = pds.read_excel(r'C:\Users\Gustavo Gomes\Documents\Programação\Meus-Projetos\Mapa Interativo\Long e Alt - Cidades\test.xlsx')
+#print(ler) plota a tabela
+ler2 = gpds.tools.geocode(ler['LOCALIDADE'], provider = "nominatim", user_agent="Intro Geocode")
+
+c = 'DADOS: QUANTIDADE DE ALUNOS EM CADA CIDADE'
+print('-=-'*20)
+print(f'{c:-^70}')
+print(ler)
+
+a = 'LONGITUDE'
+print(f'{a:-^70}')
+lista = str(ler2['geometry'].x).split()
+for i in range(len(lista)-1):
+    if i%2 == 1:
+        print(lista[i])
+
+b = 'ALTITUDE'
+print(f'{b:-^70}')
+lista = str(ler2['geometry'].y).split()
+for i in range(len(lista)-1):
+    if i%2 == 1:
+        print(lista[i])        
